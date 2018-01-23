a80abfcc0564191cc5c86d56ec6d35ed6d058e0e
SANDRO PEREIRA DOS REIS <08929532845@jpmorgan.sp>
2018-01-23 21:53:18 -0200

    Melhorando o retorno qdo não encontra nada

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using ForumApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,26 @@ namespace ForumApi.Controllers
         [HttpGet]
         public IEnumerable<Usuario> Listar(){
            return dusuario.ListarUsuario();          
+        }
+
+        /* 
+        [HttpGet("{id}")]
+        public Usuario Listar(int id){ //para buscar um unico usuário em específio
+            return dusuario.ListarUsuario().Where(x => x.Id == id).FirstOrDefault();
+            //se ele realmente encontrar o dado ele vai me retornar verdadeiro
+        }*/
+        [HttpGet("{id}")] //serve para retornar algo quando eu não pasar nada na url
+        public IActionResult Listar(int id){ 
+            var rs =new JsonResult(dusuario.ListarUsuario().Where(x => x.Id == id).FirstOrDefault());
+            rs.ContentType ="application/json";
+            if(rs.Value==null){
+            rs.StatusCode = 204;
+            rs.Value = "Não há resulatado para essa pesquisa";
+            }
+            else{
+                rs.StatusCode = 200;
+            }
+            return Json(rs);
         }
 
         [HttpPost]
