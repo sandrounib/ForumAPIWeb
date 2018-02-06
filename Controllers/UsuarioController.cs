ed3b99041ed60ba7a72736ac780d10c8b05ed991
SANDRO PEREIRA DOS REIS <08929532845@jpmorgan.sp>
2018-02-06 21:46:04 -0200

    Acrescentado Documentação Swagger

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -11,7 +11,17 @@ namespace ForumApi.Controllers
 
         DaoUsuario dusuario = new DaoUsuario();
 
+        /// <summary>
+        /// 
+        /// Retorna lista de usuários
+        /// </summary>
+        /// <returns>Lista de usuários</returns>
+        /// <response code="200">Retorna o tipo do campo da tabela usuário.</response>
+        /// <response code="400">Ocorreu um erro</response>
+
         [HttpGet]
+        [ProducesResponseType(typeof(List<Usuario>),200)]
+        [ProducesResponseType(typeof(string),400)]
         public IEnumerable<Usuario> Listar(){
            return dusuario.ListarUsuario();          
         }
@@ -22,21 +32,50 @@ namespace ForumApi.Controllers
             return dusuario.ListarUsuario().Where(x => x.Id == id).FirstOrDefault();
             //se ele realmente encontrar o dado ele vai me retornar verdadeiro
         }*/
+        /// <summary>
+        /// Busca um usuário pelo seu Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Retorna um usuário</returns>
+        /// <response code="200">Retorna um usuário</response>
+        /// <response code="400">Ocorreu um erro</response>
+        /// <response code="404">Usuário não encontrado</response>
         [HttpGet("{id}")] //serve para retornar algo quando eu não pasar nada na url
+        [ProducesResponseType(typeof(Usuario),200)]
+        [ProducesResponseType(typeof(string),400)]
+        [ProducesResponseType(typeof(string),404)]
         public IActionResult Listar(int id){ 
             var rs =new JsonResult(dusuario.ListarUsuario().Where(x => x.Id == id).FirstOrDefault());
             rs.ContentType ="application/json";
             if(rs.Value==null){
-            rs.StatusCode = 204;
+            rs.StatusCode = 404;
             rs.Value = $"Resultado para id: {id} não retornou dados";
+            return NotFound(Json(rs));
             }
             else{
                 rs.StatusCode = 200;
             }
-            return Json(rs);
+            return Ok(Json(rs));
         }
 
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="usuario">Novo usuário para registro</param>
+        /// <remarks>
+        /// Modelo de dados que deve ser enviado para cadastrar o usuário request:
+        /// 
+        ///     POST /Usuario
+        ///     {
+        ///         "nome" : "nome do usuario"
+        ///     }    
+        /// </remarks>
+        /// <response code="200">Retorna o usuario cadastrado</response>
+        /// <response code="400">Ocorreu um erro</response>
+
         [HttpPost]
+        [ProducesResponseType(typeof(Usuario),200)]
+        [ProducesResponseType(typeof(string),400)]
         public void Adicionar([FromBody] Usuario usuario){
             dusuario.Cadastro(usuario);
         }
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ForumApi
 {
@@ -15,7 +17,30 @@ namespace ForumApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc();            
+
+            //Metodo para usar a documentação Swagger
+            // para mostrar de o dotnet run e abra o browser com
+            //http://localhost:5000/swagger/
+            services.AddSwaggerGen(c =>{
+                c.SwaggerDoc("V1",new Info{
+                    Version = "V1",
+                    Title = "ApiForumApi",
+                    Description = "Documentação da Api do Forum Api",
+                    TermsOfService = "None",
+                    Contact =  new Contact {
+                    Name = "Sandro Reis",
+                    Email = "sandrounib@hotmail.com",
+                    Url = "http://www.corujasdev.com.br"
+                    } 
+                }); 
+
+                //codigo para usar a documentação da api que foi colocado ///
+                var caminhoBase = AppContext.BaseDirectory;
+                var caminhoXml =  Path.Combine(caminhoBase,"ApiForumApi.xml");
+
+                c.IncludeXmlComments(caminhoXml);           
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +50,12 @@ namespace ForumApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //Usado para documentação swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>{
+                c.SwaggerEndpoint("/swagger/V1/swagger.json","ApidoForumApi");
+            });
             
             app.UseMvc();
 
